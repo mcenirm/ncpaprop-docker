b475ad442f9691de6dd52084ef8f859b89b5b700
Michael McEniry <mcenirm@mceniry.net>
2020-05-30 21:31:59 -0500

    Add petsc build script

---
+++ build-petsc/build-with-scalar-type.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+
+./configure \
+    PETSC_ARCH=arch-linux-c-$1 \
+    --with-scalar-type=$1 \
+    --with-fc=0 --with-debugging=0 --with-mpi=0 --download-f2cblaslapack=1
+make PETSC_ARCH=arch-linux-c-$1 all
+make PETSC_ARCH=arch-linux-c-$1 test
+make distclean
